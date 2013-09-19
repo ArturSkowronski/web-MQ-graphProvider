076d6bb555c757d65b8d3b540229301afd695752
Articles <articles.pl@gmail.com>
2013-09-19 23:56:35 +0200

    Added Redis

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express=require("express");
 var app=express();
 
+
 app.use(express.logger());
 
 app.configure(function(){
@@ -10,9 +11,25 @@ app.configure(function(){
 
 app.use("/public", express.static(__dirname + '/public'));
 
+
+var redisURL="redis://redistogo:d1c1fc905ceb6da99dd8fb014f5dbcb5@jack.redistogo.com:9513";
+if (redisURL) {
+    var rtg   = require("url").parse(redisURL);
+    var redis = require("redis").createClient(rtg.port, rtg.hostname);
+    redis.auth(rtg.auth.split(":")[1]);
+} else {
+    var redis = require("redis").createClient();
+
+}
+
 app.get('/', function(req, res){
+    redis.get("foo", function(err, value) {
+        console.log("redisZSerwera: "+value);
+    });
     res.render('index', {option: 'value'});
-});  
+});
+
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
