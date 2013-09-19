0c8517c75e983bf004de4021ce142a6fa5174fd6
Articles <articles.pl@gmail.com>
2013-09-19 18:55:10 +0200

    repairing dynos

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-web: java $JAVA_OPTS -jar web/target/dependency/webapp-runner.jar --port $PORT web/target/*.war
-worker: sh worker/target/bin/worker
+web: node server.js
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express=require("express");
 var app=express();
 
+app.use(express.logger());
+
 app.configure(function(){
     app.set('views', __dirname + '/views');
     app.set('view engine', 'jade');
